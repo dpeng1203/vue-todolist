2bafad951cefd40aa0afb00b03eb1a6ad0b5668f
dpeng1203 <d_p1203@163.com>
2018-05-16 13:57:56 +0800

    分成三大块

---
+++ src/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <topbar class="title"></topbar>
+    <main>
+      <editor class="et"></editor>
+      <preview class="view"></preview>
+    </main>
+  </div>
+</template>
+
+<script>
+import Editor from './components/Editor'
+import Preview from './components/Preview'
+import Topbar from './components/Topbar'
+
+export default {
+  components: {
+    'topbar': Topbar,
+    'preview': Preview,
+    'editor': Editor
+  }
+}
+</script>
+<style>
+#app{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.title{
+  background: red;
+}
+main{
+  background: green;
+  display: flex;
+  flex: 1;
+}
+main > .et{
+  width: 20em;
+}
+main > .preview{
+  flex: 1;
+}
+
+</style>
