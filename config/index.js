97741a4c9d39a18777d162643976b82cfa50bf05
dpeng1203 <d_p1203@163.com>
2018-05-17 15:40:30 +0800

    添加dist

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-todolist/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
